80c10801f73874c33bed2b0743e0f3df82e5a093
jaskinner <jon.a.skinner1@gmail.com>
2023-11-13 16:48:46 -0500

    first commit

---
+++ src/main.js
@@ -0,0 +1,23 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import axios from "axios";
+
+import App from './App.vue'
+import router from './router'
+
+import './assets/scss/styles.scss'
+import 'bootstrap';
+
+const instance = axios.create({
+	baseURL: 'http://localhost:3300',
+	timeout: 1000,
+	headers: { 'Content-Type': 'application/json' }
+});
+
+const app = createApp(App)
+
+app.use(createPinia())
+
+app.provide('$axios', instance);
+app.use(router)
+app.mount('#app')
