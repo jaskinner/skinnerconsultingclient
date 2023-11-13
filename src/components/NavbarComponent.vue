c62ec7bcf7a669322b9294b9115af598d4ab6c27
jaskinner <jon.a.skinner1@gmail.com>
2023-11-13 18:24:22 -0500

    adding sections

---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <nav class="navbar navbar-expand-md">
+        <div>
+            <a class="navbar-brand" href="#">Skinner Consulting</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNav"
+                aria-controls="mainNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div id="mainNav" class="collapse navbar-collapse">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
